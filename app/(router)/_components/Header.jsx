7232cfadeb1ee8d22872b8c495a0349e9a683ffc
AleAndino <aleandino.cbaa@gmail.com>
2024-02-21 22:06:38 -0300

    header

--- app/(router)/_components/Header.jsx
+++ app/(router)/_components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Search } from 'lucide-react';
+import { BellRing } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
-    <div className='p-4 bg-white flex'>
+    <div className='p-4 bg-white flex justify-between'>
       <div className='flex items-center border rounded-md pl-2'>
-        <Search className='h-5 w-5'/>
+        <Search className='h-5 w-5' />
         <input className='p-2 outline-none' type='text' placeholder='Search...' />
+      </div>
+      <div className='flex items-center gap-4'>
+      <BellRing className='text-gray-500'/>
+      <Button>Get Started</Button>
       </div>
     </div>
   )
--- app/(router)/courses/page.js
+++ app/(router)/courses/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Courses() {
   return (
-    <div className=''>Courses</div>
-  )
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      {/* Left container */}
+      <div className="col-span-2">
+        Banner
+      </div>
+      {/* Right container */}
+      <div>
+        Right section
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
